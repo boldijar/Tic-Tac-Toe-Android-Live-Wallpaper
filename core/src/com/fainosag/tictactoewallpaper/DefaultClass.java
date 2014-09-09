40131120d7631adbde6cf222076061440991e522
Boldijar Paul <paul.bv96@yahoo.com>
2014-09-09 10:43:20 +0300

    Added GameScreen class

--- core/src/com/fainosag/tictactoewallpaper/DefaultClass.java
+++ core/src/com/fainosag/tictactoewallpaper/DefaultClass.java
@@ -1,28 +1,14 @@
 package com.fainosag.tictactoewallpaper;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Game;
+import com.fainosag.tictactoewallpaper.screens.GameScreen;
 
-public class DefaultClass extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	// test
+public class DefaultClass extends Game {
 	@Override
 	public void create () {
-        //
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+        // se up our game screen
+        setScreen(new GameScreen());
+    }
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+
 }
