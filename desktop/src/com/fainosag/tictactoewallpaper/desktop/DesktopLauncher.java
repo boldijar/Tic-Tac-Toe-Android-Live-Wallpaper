6104d4d5cb295a2ac8e9469a744230aad9e258b4
Boldijar Paul <paul.bv96@yahoo.com>
2014-09-09 10:32:31 +0300

    Initial commit

---
+++ desktop/src/com/fainosag/tictactoewallpaper/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.fainosag.tictactoewallpaper.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.fainosag.tictactoewallpaper.DefaultClass;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new DefaultClass(), config);
+	}
+}
